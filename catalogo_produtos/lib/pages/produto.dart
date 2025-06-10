265908f6c0d91f71324ce6e8e35b92994e1c5a9c
Miguel <miguelcalixtomendes@gmail.com>
2025-06-10 20:18:48 -0300

    feat: repo init

---
+++ catalogo_produtos/lib/pages/produto.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+abstract class ProdutoWidget extends StatelessWidget {
+  final String nome;
+  final double preco;
+  final String descricao;
+  final String imagemUrl;
+  final VoidCallback onTap;
+  
+  const ProdutoWidget({
+    super.key,
+    required this.nome,
+    required this.preco,
+    required this.descricao,
+    required this.imagemUrl,
+    required this.onTap,
+  });
+
+  String getTipoProduto();
+}
